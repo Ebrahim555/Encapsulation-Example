ad40ddbf5441a2c88627cb1ffa395a626cdbf209
Md Ebrahim Hossain <69958100+Ebrahim555@users.noreply.github.com>
2020-08-28 02:42:03 +0600

    Encapsulation demo in java

---
+++ Encapsulation108.java
@@ -0,0 +1,15 @@
+
+package Method81;
+
+public class Encapsulation108 {
+    private String name;
+ private int age;
+ public void setname(String name){
+     this.name=name;
+     
+ }
+ public String getName(){
+     
+return name;     
+ }
+}
---
+++ Test108.java
@@ -0,0 +1,14 @@
+
+package Method81;
+
+public class Test108 {
+    public static void main(String[] args) {
+        Encapsulation108 pi=new Encapsulation108();
+        pi.setname("Name :ebrahim");
+    
+        System.out.println( pi.getName());
+    
+    }
+    
+  
+}
